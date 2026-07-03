e9afb2a755891d7d6b5b5862e766b5db61462c10
Andre Ferdi <email_andre@gmail.com>
2026-07-04 03:59:57 +0700

    fresh build uas fix

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const sequelize = new Sequelize(
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  }
+);
