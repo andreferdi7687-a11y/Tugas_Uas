3178012cb0a365d852df0fbff8b504b768ce1d63
Andre Ferdi <email_andre@gmail.com>
2026-07-04 04:07:19 +0700

    fix referenceerror sequelize defined

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
+// 1. PASTIKAN BARIS INI ADA DI PALING ATAS FILE, LEK!
+const { Sequelize } = require('sequelize'); 
+
+// 2. Baru di bawahnya masukkan konfigurasi koneksi cloud kita tadi
 const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     port: process.env.DB_PORT,
-    
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false
@@ -14,3 +17,5 @@ const sequelize = new Sequelize(
     }
   }
 );
+
+module.exports = sequelize;
