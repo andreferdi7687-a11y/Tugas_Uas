1b416d34c3ecd242f098636f268df98e7fc7e01d
Andre Ferdi <email_andre@gmail.com>
2026-07-04 04:21:16 +0700

    fix referenceerror datatypes in product model

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 const sequelize = require('../config/db');
-
+const { DataTypes } = require('sequelize'); 
 
 const Product = sequelize.define('Product', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
